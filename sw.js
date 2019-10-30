8cad61b5d50cb39ef466343dbb9ad3db38e0dd24
Jaakko Juvonen <jaakko.juvonen@wunderdog.fi>
2019-10-30 16:30:33 +0200

    Updates

---
+++ sw.js
@@ -0,0 +1,733 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+workbox.core.skipWaiting();
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "about-us-hero-new-thumb.113974d8.jpg",
+    "revision": "3bce8a91bee9a79055c1a4b2b457300d"
+  },
+  {
+    "url": "about-us-hero-new.976a3d68.jpg",
+    "revision": "63567b9bda7d32a387549a55ceaf60b9"
+  },
+  {
+    "url": "AboutUs.f44af004.js",
+    "revision": "28d6292dd71ac72ff66f94798d1f138e"
+  },
+  {
+    "url": "android-chrome-192x192.77e150f6.png",
+    "revision": "d8f6349966461f4045dc2c5cb86d9726"
+  },
+  {
+    "url": "android-chrome-384x384.48c0229a.png",
+    "revision": "304723905efde579b7f9b5fcca363804"
+  },
+  {
+    "url": "AnnaMariaLaFiura.6dfbc254.js",
+    "revision": "17fab54274132841c1ac7977db0f03b1"
+  },
+  {
+    "url": "AnteroTorhonen.79a0734e.js",
+    "revision": "6d5c9ca3c8d2c6362e72dbdfeb19d787"
+  },
+  {
+    "url": "apple-touch-icon.0d634279.png",
+    "revision": "924faf2cc4a2c9aad9b5f52ea7205b34"
+  },
+  {
+    "url": "Berlin.468b0beb.js",
+    "revision": "5acf3f2bb1ccd1c5550b2987beb9180d"
+  },
+  {
+    "url": "bg-thumb.49e63dcc.jpg",
+    "revision": "d13dd8c4dfddc51b8625d1e8c995a9dd"
+  },
+  {
+    "url": "bg.91c7b912.jpg",
+    "revision": "27fa63f30355078915464f96459712a9"
+  },
+  {
+    "url": "blueprint-card-thumb.ac90c4b9.jpg",
+    "revision": "10c271603618643dd120f910fa7eba2f"
+  },
+  {
+    "url": "blueprint-hero-thumb.4d3113f8.jpg",
+    "revision": "06feaf79985a8fa4ae3bef3355cbc77f"
+  },
+  {
+    "url": "blueprint-hero.7b2d81ab.png",
+    "revision": "71223604afc6728adb852a9dd2c25d5c"
+  },
+  {
+    "url": "blueprint-photo1-thumb.b837d077.jpg",
+    "revision": "a8b63f64249259ac766dd4e8c88ab41f"
+  },
+  {
+    "url": "blueprint-photo1.320d99b7.jpg",
+    "revision": "7e6373e2a6087f4a35a415e0d9cb6808"
+  },
+  {
+    "url": "blueprint-photo2-thumb.f643fc92.jpg",
+    "revision": "35096415730ad01b4ba911a6576b614d"
+  },
+  {
+    "url": "blueprint-photo2.d9fe2bfd.jpg",
+    "revision": "6ca78acd23a43835544668dc1ea8fbce"
+  },
+  {
+    "url": "blueprint-photo3-thumb.469f43bd.jpg",
+    "revision": "97cc8a1a9dbab3c87facfeb9e242acf8"
+  },
+  {
+    "url": "blueprint-photo3.58f04c6c.jpg",
+    "revision": "087be1f4c37d1cb279c8d848e7b03b63"
+  },
+  {
+    "url": "blueprint-photo4-thumb.8c3840dd.jpg",
+    "revision": "1c681aeccd8c49ea71c26ea906ee1324"
+  },
+  {
+    "url": "blueprint-photo4.75089d1c.jpg",
+    "revision": "ba0b2594d2287667795a59cc053b3b26"
+  },
+  {
+    "url": "blueprint-photo5-thumb.9a01b2f3.jpg",
+    "revision": "c7a84de7c59aecd861efaaeccfc3dc6a"
+  },
+  {
+    "url": "blueprint-photo5.8d7c88ee.jpg",
+    "revision": "e1b754362006dab49cdf3269d8d7b834"
+  },
+  {
+    "url": "blueprint-photo6-thumb.0a3ad1c2.jpg",
+    "revision": "5a654aa0e89d91d3ae05bf9d23cc733e"
+  },
+  {
+    "url": "blueprint-photo6.cd164fce.jpg",
+    "revision": "8c89e3be3aded35234d3e2aa4ce5cc60"
+  },
+  {
+    "url": "BlueprintGenetics.62758d5c.js",
+    "revision": "5ab93b7b6515dcf33edb70b7d2627294"
+  },
+  {
+    "url": "BluePrintGenetics.763b6532.js",
+    "revision": "7e20f49991e5d0b5b6f2a3daaffe74c3"
+  },
+  {
+    "url": "career-hero-new-thumb.c7b8934f.jpg",
+    "revision": "aeb2c0a73f329f8750a8b28e40d9da31"
+  },
+  {
+    "url": "career-hero-new.4703ba47.jpg",
+    "revision": "3e911d6825421b26b15e3a35a15456ad"
+  },
+  {
+    "url": "career-photo1-thumb.98e8d69c.jpg",
+    "revision": "6d3f2f85171bd9758accea9ba0ec1de5"
+  },
+  {
+    "url": "career-photo1.3e97d710.png",
+    "revision": "61edcbda8cd5dfad6a1ecb274a479b11"
+  },
+  {
+    "url": "career-photo2-thumb.6989f080.jpg",
+    "revision": "981f505e21e92bcb7713d4978c0bd075"
+  },
+  {
+    "url": "career-photo2.f1e45474.png",
+    "revision": "c4dd1c4973840f94203291a6817bd0e8"
+  },
+  {
+    "url": "career-photo3-thumb.631c60b8.jpg",
+    "revision": "c7392f1673274fbb4f806ac26fa694be"
+  },
+  {
+    "url": "career-photo3.2b4fc43d.png",
+    "revision": "2508aea055a2d67440028997bbb04896"
+  },
+  {
+    "url": "career-photo4-thumb.7502cc7a.jpg",
+    "revision": "21da7b860615aacc8a7533ae3fc7a948"
+  },
+  {
+    "url": "career-photo4.5411081f.jpg",
+    "revision": "c6dec112c07ba13d346281e0d837a84f"
+  },
+  {
+    "url": "career-photo5-thumb.86aeb3a1.jpg",
+    "revision": "8d230491c7f3f3ead09aac3b3782c295"
+  },
+  {
+    "url": "career-photo5.2cae34dd.jpg",
+    "revision": "f16d5aa60d7bccb040dc80d43e427fcf"
+  },
+  {
+    "url": "Career.2001383d.css",
+    "revision": "5fa54155b9424719ad36612ee8e663a1"
+  },
+  {
+    "url": "Career.9c628013.js",
+    "revision": "7a5206af5d28242d45d8167b30a5e8e9"
+  },
+  {
+    "url": "CareerBlogPostsContent.6e38f472.js",
+    "revision": "1dbd6f62d1ddf31268f176b6ba0acbaa"
+  },
+  {
+    "url": "Clients.7ca691f2.css",
+    "revision": "fa2694c20a164ddde25aeaaec3d8972e"
+  },
+  {
+    "url": "Clients.a61ceb17.js",
+    "revision": "92bc164cc42f73aff2663139abd52ec4"
+  },
+  {
+    "url": "Contact.a751181f.js",
+    "revision": "e9aab14886a965ca2ab0e9aa28a9c45e"
+  },
+  {
+    "url": "Contact.a805f7b4.css",
+    "revision": "8368e47eaa4cd0f4a64a6f7d3e174899"
+  },
+  {
+    "url": "CookieDisclamer.984166f4.js",
+    "revision": "a71654f7f5cc4d8fdb7b1f5f28dc5171"
+  },
+  {
+    "url": "Cookies.3f40544d.js",
+    "revision": "810076495b1ad40dd13ebf22aa2ed14a"
+  },
+  {
+    "url": "ElectronicInvoicing.c479fadf.js",
+    "revision": "711d03a94294783c0db169c384da7afb"
+  },
+  {
+    "url": "favicon-16x16.5df9168a.png",
+    "revision": "4d07f747223b11fb3743a0f5e37282c5"
+  },
+  {
+    "url": "favicon-32x32.7ee20a3e.png",
+    "revision": "f32a86f2adc971da32f12d9599b200d5"
+  },
+  {
+    "url": "FiudenDemo-hero-thumb.e85473f3.jpg",
+    "revision": "b96e7908013c59e987f104500fb39123"
+  },
+  {
+    "url": "FiudenDemo-hero.c1f91bde.png",
+    "revision": "c636fac5be1b8266bf8b8ff3067700b4"
+  },
+  {
+    "url": "FiudenDemo.37db2a94.js",
+    "revision": "295e110d32850ec2fbfb0af17474ef3e"
+  },
+  {
+    "url": "FooterBottom.4ec992df.css",
+    "revision": "e176c377413114604b02ea7187fa1db0"
+  },
+  {
+    "url": "FooterBottom.9a94ae38.js",
+    "revision": "48f90dbf26c93cd263f787db9eb37be9"
+  },
+  {
+    "url": "foreground-thumb.41089490.png",
+    "revision": "4a4a04c9bf378b871c86a717ecdc47d7"
+  },
+  {
+    "url": "foreground.f73d0f04.png",
+    "revision": "da8faa6070598d4134797353d302c0bb"
+  },
+  {
+    "url": "FunStats.ca545a7f.js",
+    "revision": "d029db916a8336b1acc26d22c39eb110"
+  },
+  {
+    "url": "Helsinki.ebaece65.js",
+    "revision": "816ec5ec101aee8bc0c248a674692d29"
+  },
+  {
+    "url": "Hero.9fe2271f.js",
+    "revision": "8da955a47718ca211eedd4b66efa8205"
+  },
+  {
+    "url": "HomeBlogPostsContent.f6b028eb.js",
+    "revision": "a941ad2e742bddf3a815f67ad28be86c"
+  },
+  {
+    "url": "HomeCaseCards.783b4764.js",
+    "revision": "311140caf35edaf1643af9ecf4771f96"
+  },
+  {
+    "url": "index.html",
+    "revision": "0eeed164d491dff4519cf988def17afc"
+  },
+  {
+    "url": "JasperVanGhemen.271422ec.js",
+    "revision": "da955dd9997cdb9bc10a5992bdd6d600"
+  },
+  {
+    "url": "JoannaRindell.ac958baf.js",
+    "revision": "03b028c014694f354ec1e36763fc6f8e"
+  },
+  {
+    "url": "JoinThePack.347040a2.js",
+    "revision": "97c0042a91a47df7c565da1eccd04e45"
+  },
+  {
+    "url": "LauriPaalama.764e57cd.js",
+    "revision": "a4279a6898a065e6e8e5aa68a2c9681b"
+  },
+  {
+    "url": "LetsPlay.ac2dcc0f.js",
+    "revision": "13e91341ac7229fc7b173972b0f549a2"
+  },
+  {
+    "url": "LetsTalk.8bc281a0.js",
+    "revision": "a672188671bb84b11574f9e27dd0a2cd"
+  },
+  {
+    "url": "logo-alma.f3e51502.png",
+    "revision": "e3625248f79d4616fb8492b4c3ad9842"
+  },
+  {
+    "url": "logo-barona.06d1613c.png",
+    "revision": "2a24e2d19d6c8265b754acad4f1d80fc"
+  },
+  {
+    "url": "logo-lt.fc775bdd.png",
+    "revision": "6d725b838fcfdab41261973b868e35e7"
+  },
+  {
+    "url": "logo-perusterveys.0f9cf871.png",
+    "revision": "ebb583982219771fa06ea94efd64141e"
+  },
+  {
+    "url": "logo-redbull.009f6aa0.png",
+    "revision": "c8bb331adbd3d595b37a706002dd8f9b"
+  },
+  {
+    "url": "logo-rovio.057310ee.png",
+    "revision": "bc5fd761321ea2a6cef2f1cd7bde84ba"
+  },
+  {
+    "url": "logo-sanoma-pro.3c04d18e.png",
+    "revision": "ee14b1180de1838b87d2afb9703c35f0"
+  },
+  {
+    "url": "logo-smartly.27c8cf6b.png",
+    "revision": "88091bd5a2f85733cbc7ba6bd4627566"
+  },
+  {
+    "url": "Lund.f4d894cd.js",
+    "revision": "3c90c918e623267d5c0ccd9029423746"
+  },
+  {
+    "url": "MainNav.1da0399d.js",
+    "revision": "dd46dfadf088ea35ae1691cf26f213ca"
+  },
+  {
+    "url": "MartinLyckestjarn.1c255b3c.js",
+    "revision": "27e3ab40c69c2411f4d8aaadb522d705"
+  },
+  {
+    "url": "MeriTuuliOlkkonen.ae7af590.js",
+    "revision": "d24313671dd4313c7523ab716ca4b4c7"
+  },
+  {
+    "url": "mieli-card-thumb.e00453ed.jpg",
+    "revision": "7b4dee6fbb2dd2521437809eb4e0cd2f"
+  },
+  {
+    "url": "mieli-card.4b94e270.jpg",
+    "revision": "4b9895ce7514a8b7ec7f436691f32761"
+  },
+  {
+    "url": "mieli-hero-thumb.3f59b75f.jpg",
+    "revision": "e9fb5ad66d6581554f74878cd64773ff"
+  },
+  {
+    "url": "mieli-hero.f36da66d.jpg",
+    "revision": "fe10a29cb23b2f0b6001b2b96852af51"
+  },
+  {
+    "url": "mieli-photo1-thumb.2e1027ec.jpg",
+    "revision": "8bde04690ecb2bbee48ed7cc07d06477"
+  },
+  {
+    "url": "mieli-photo1.5e107ec3.jpg",
+    "revision": "2ce3cb737bbd59ca0853cd9d7dac3e96"
+  },
+  {
+    "url": "mieli-photo2-thumb.c724368c.jpg",
+    "revision": "ecc1bd13d9e338167b7fd541118d39eb"
+  },
+  {
+    "url": "mieli-photo2.d85c5c50.jpg",
+    "revision": "0d84c398153d7e9690ca7c3b169b13d4"
+  },
+  {
+    "url": "mieli-photo3-thumb.2b8bcfc8.jpg",
+    "revision": "1bf2bec11881a5b6b0680cc99ff59fce"
+  },
+  {
+    "url": "mieli-photo3.b13f0d0c.jpg",
+    "revision": "ffae6f496f2619918ae8accad6ff08c9"
+  },
+  {
+    "url": "mieli-photo4-thumb.bcec2291.jpg",
+    "revision": "8eece1833593a6e056a6f091b8a987ce"
+  },
+  {
+    "url": "mieli-photo4.686397f0.jpg",
+    "revision": "d218f9a2fac1e508eafbfb84e16d8dd7"
+  },
+  {
+    "url": "Mieli.9ffcbe54.js",
+    "revision": "671de4eda1b8c9092eeab6541bb8231c"
+  },
+  {
+    "url": "Mieli.cabd17d5.js",
+    "revision": "5b577d070f37349b92c8e837c2090629"
+  },
+  {
+    "url": "MikaViskari.a17e33d2.js",
+    "revision": "bfef6efa67ced32f39c3e616f18a7324"
+  },
+  {
+    "url": "mikko-frames-thumb.a4646cde.png",
+    "revision": "a2596c4bf1f52d8b011f9aaf2540c312"
+  },
+  {
+    "url": "mikko-frames.128d23ab.png",
+    "revision": "8d2e3c91d71f1178ac62b34406e30267"
+  },
+  {
+    "url": "NotFound.4ae08fe4.js",
+    "revision": "366d74c9e91654bf0680e137c6a4ed2c"
+  },
+  {
+    "url": "oodi-card-thumb.bd5fe87b.jpg",
+    "revision": "78ca4032a32bb13ebd19aecdc66e9dd5"
+  },
+  {
+    "url": "oodi-card.33071d6e.png",
+    "revision": "b9c5296b3f6a3081805ca49855ddbb2f"
+  },
+  {
+    "url": "oodi-hero-thumb.f40d00ba.jpg",
+    "revision": "fb8f73fc4e811536c714c84379a5f851"
+  },
+  {
+    "url": "oodi-hero.fd4b76f9.png",
+    "revision": "520678b9dc77f57572546ead5baa26a7"
+  },
+  {
+    "url": "oodi-photo1-thumb.925e229c.jpg",
+    "revision": "cdc2458800ee066bbc6e5933acb49a5f"
+  },
+  {
+    "url": "oodi-photo1.caf93e00.png",
+    "revision": "1f250dd45a616e87d1dd1d5039bc0fa4"
+  },
+  {
+    "url": "oodi-photo2-thumb.bb1b842e.jpg",
+    "revision": "b27d04d38389cbb1263c80ddc9e2813a"
+  },
+  {
+    "url": "oodi-photo2.76970668.png",
+    "revision": "a921ff7d6c86151e2e9ffb5aa43f77a4"
+  },
+  {
+    "url": "Oodi.064362ae.js",
+    "revision": "79a8da65a662f101c8f76d8337f743e1"
+  },
+  {
+    "url": "Oodi.61a5db90.js",
+    "revision": "d7c6aa4653d79c6e452ef34e64f1e869"
+  },
+  {
+    "url": "OpenPositions.e75b5ccc.js",
+    "revision": "a47fc94415bbbaa27d6ed00d266406dd"
+  },
+  {
+    "url": "OpenPositions2.6977e90d.js",
+    "revision": "028c9115a18670cb8281a352e9d4e560"
+  },
+  {
+    "url": "pahkina-thumb.049fa2e7.jpg",
+    "revision": "ef4d8e5ccbad1414d7dff956771de59f"
+  },
+  {
+    "url": "Pahkina.2ac52bcd.js",
+    "revision": "7c83dc96d0615652acd9956836674a3b"
+  },
+  {
+    "url": "pahkina.957a39d3.jpg",
+    "revision": "1101bcd799f536f952a978a69899f8d7"
+  },
+  {
+    "url": "Pahkina.f67988e6.css",
+    "revision": "2417867f773ca4216ecf939d6ff8ca59"
+  },
+  {
+    "url": "perusterveys-card-thumb.a5a31220.jpg",
+    "revision": "41d168ce9f281fdd8b57c00b0b838753"
+  },
+  {
+    "url": "perusterveys-card.29d71092.png",
+    "revision": "dd7557b00c1aea0942a2389318b24344"
+  },
+  {
+    "url": "perusterveys-hero-thumb.db7fd02c.jpg",
+    "revision": "d6aaf15489ee74b2e19c46a33f7b4187"
+  },
+  {
+    "url": "perusterveys-hero.83f34cd7.png",
+    "revision": "8805acccc11163fe4ce1a98b9154c511"
+  },
+  {
+    "url": "perusterveys-photo-thumb.155b7774.jpg",
+    "revision": "1b8556d7f16a168b1b09e6994754be9b"
+  },
+  {
+    "url": "perusterveys-photo.47ad7065.png",
+    "revision": "0ae7cae27acdbeb038908c3cc5076382"
+  },
+  {
+    "url": "PerusTerveys.364100ab.js",
+    "revision": "188b9aa0cdb6c7b6d2b325f8bba6a278"
+  },
+  {
+    "url": "PerusTerveys.bd727a97.js",
+    "revision": "e4642024a8c942dcabbe1e1da6a4bd01"
+  },
+  {
+    "url": "pexraytech-card-thumb.3c61b2de.jpg",
+    "revision": "372e868beb14311f599d3d7ecb4aa46a"
+  },
+  {
+    "url": "pexraytech-card.1c53e03e.png",
+    "revision": "fc340a0d840194c8c761a47ad41afd6a"
+  },
+  {
+    "url": "pexraytech-hero-thumb.e439c742.jpg",
+    "revision": "a3b84dcff2876b8bd5479b1bb59ac639"
+  },
+  {
+    "url": "pexraytech-hero.3a36716a.png",
+    "revision": "19a358d60de71c6ee1d1d8cf1786590c"
+  },
+  {
+    "url": "pexraytech-mockup-thumb.cac99865.jpg",
+    "revision": "1faeefc38a56164e96d88db3c47c134f"
+  },
+  {
+    "url": "pexraytech-mockup.b9dfb935.png",
+    "revision": "511ee9bca6069e0f87410d7df168b17e"
+  },
+  {
+    "url": "Pexraytech.705381bd.js",
+    "revision": "6619136849b83648cc52681092cd3b4a"
+  },
+  {
+    "url": "Pexraytech.973e9119.js",
+    "revision": "39b66af69fca074b1d9f94e4ec89b748"
+  },
+  {
+    "url": "PingPong.195d08eb.js",
+    "revision": "1c501318a6646bdf77ae2f2e9853f3e3"
+  },
+  {
+    "url": "PingPong.a9c49f90.css",
+    "revision": "e64a173ab768a4ad4ba6b007ed7e4e42"
+  },
+  {
+    "url": "pirjo-frames-thumb.d39695c4.png",
+    "revision": "cfc7871d9cbbfbad57fc03d63fcd3eef"
+  },
+  {
+    "url": "pirjo-frames.1f507a96.png",
+    "revision": "7263fd2fa84456e90be4450248a0e4ee"
+  },
+  {
+    "url": "Privacy.9ad077aa.js",
+    "revision": "4566adf83745f500b0ebad79bd96ac39"
+  },
+  {
+    "url": "pumpum-photo1-thumb.0a29b456.jpg",
+    "revision": "4846a37d07a20c285b4ccf815a2cef85"
+  },
+  {
+    "url": "pumpum-photo1.7488df46.png",
+    "revision": "d978764c1ba5e0445d4d9cf9c326035d"
+  },
+  {
+    "url": "pumpum-photo2-thumb.01b71824.jpg",
+    "revision": "4d21cf0ad21f67d7e50f8935befc6676"
+  },
+  {
+    "url": "pumpum-photo2.ad14c58c.png",
+    "revision": "efb21e8d3db3994aee19cc4af9b903f2"
+  },
+  {
+    "url": "PumPum.272ed62e.css",
+    "revision": "309c39406d01b0788e9bd8f9bee4d80d"
+  },
+  {
+    "url": "PumPum.3995bbd6.js",
+    "revision": "89bfa96dea19667627bf5fb2843d7084"
+  },
+  {
+    "url": "PumpumStats.a8b7b3ba.js",
+    "revision": "2a953acddeec40838013a75f694d3ae6"
+  },
+  {
+    "url": "redbull-card-thumb.53908546.jpg",
+    "revision": "0a0a02ab435a5afc0a2611e305ed5dae"
+  },
+  {
+    "url": "redbull-card.d6c01910.png",
+    "revision": "4c507004d5a9f1b9ee387bb41a82f439"
+  },
+  {
+    "url": "redbull-hero-thumb.8e5356ee.jpg",
+    "revision": "8a9aabcc12a20fb2b10cfe3d3721a584"
+  },
+  {
+    "url": "redbull-hero.097c2124.png",
+    "revision": "4658485ec98d699e1dfdb6bf1b7aeaa5"
+  },
+  {
+    "url": "redbull-mockup-thumb.a8fdfdff.jpg",
+    "revision": "d28608aff573954ffdef577da55f253e"
+  },
+  {
+    "url": "redbull-mockup.cef6b22b.png",
+    "revision": "c5118e8293d27bbee7635dbb0513c4af"
+  },
+  {
+    "url": "redbull-photo1-thumb.f6ab7faf.jpg",
+    "revision": "9a7ccbab60884a4aab45bdf8c0803587"
+  },
+  {
+    "url": "redbull-photo1.f1a34cd7.png",
+    "revision": "142eaacd4ca7d323670e9a4b407beeb4"
+  },
+  {
+    "url": "redbull-photo2-thumb.326b820f.jpg",
+    "revision": "4ac977655d606e2ec4a25d896c9df27a"
+  },
+  {
+    "url": "redbull-photo2.370d3387.png",
+    "revision": "7bb575a2b21c0f1ab07f69da7009751e"
+  },
+  {
+    "url": "RedBull.9d7c0cf8.js",
+    "revision": "e30e2dfce275387bf9a872a8763e8542"
+  },
+  {
+    "url": "RedBull.c7bbbb45.js",
+    "revision": "9a58742f88bfd5873b4a22b1d20e661b"
+  },
+  {
+    "url": "RedhaCherif.b7293e52.js",
+    "revision": "2505b5116beeba1cecc808ec0da365d6"
+  },
+  {
+    "url": "SeriousStats.b401b4e5.js",
+    "revision": "26b4bf8fef000047f4cfaa3602292235"
+  },
+  {
+    "url": "smartly-card-thumb.bb836a78.jpg",
+    "revision": "d787df56969eeda4b4e8fc3779535190"
+  },
+  {
+    "url": "smartly-card.347b6399.png",
+    "revision": "60320f3ae58f99fa1487a062bd240bab"
+  },
+  {
+    "url": "smartly-hero-thumb.e86dda65.jpg",
+    "revision": "6dc0d39c167d120236353b5d5f442cd4"
+  },
+  {
+    "url": "smartly-hero.fda15809.png",
+    "revision": "0b6ef643608711cf6807943deb69e87a"
+  },
+  {
+    "url": "smartly-photo1-thumb.422d7532.jpg",
+    "revision": "5b5ec4ff06d7a7141035264baa6a9f59"
+  },
+  {
+    "url": "smartly-photo1.6ffbc415.jpg",
+    "revision": "7996a1c027b6ec280036dc1fa03d830f"
+  },
+  {
+    "url": "smartly-photo2-thumb.11272827.jpg",
+    "revision": "79d267fa28609c664a7e147c33f362cf"
+  },
+  {
+    "url": "smartly-photo2.9e6a1861.jpg",
+    "revision": "aa0df15375d50718aba303eb800976dd"
+  },
+  {
+    "url": "smartly-photo3-thumb.6f08c61d.jpg",
+    "revision": "7c3b859a9d598c11d8f57a6d2b0c6c50"
+  },
+  {
+    "url": "smartly-photo3.3c412322.jpg",
+    "revision": "669c083cafdabed44a20f5be6bf22800"
+  },
+  {
+    "url": "Smartly.40e33d1f.js",
+    "revision": "b6d431a8f049255fa7a8e11d83967c22"
+  },
+  {
+    "url": "Smartly.4109e362.js",
+    "revision": "44746c363d70fdcb4d3b47e050f1806e"
+  },
+  {
+    "url": "socialmedia-default.98412fbb.png",
+    "revision": "89448eacc4cf577f4c3e20e285ff42ea"
+  },
+  {
+    "url": "src.5a23d5bc.js",
+    "revision": "4e30d1dff25307ac97a41f1bfa2488f2"
+  },
+  {
+    "url": "src.9ba3cf64.css",
+    "revision": "b29200b9a92dd057523c724ac8afeb18"
+  },
+  {
+    "url": "TimoSand.18173fc8.js",
+    "revision": "7f8eaf7fc86284aef6c7d301b74bb189"
+  },
+  {
+    "url": "/",
+    "revision": "b0b4a9e713d1d4a4d01734d669fb0717"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"));
